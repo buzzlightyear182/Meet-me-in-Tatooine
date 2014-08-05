6805fc3266b838e12f950f04c7faaad6feab2fdd
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-08-05 16:10:32 +0200

    Added event listener to geolocation button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,27 @@
-if (navigator.geolocation) {
-  console.log("Browser accepts geolocation");
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
-}
+document.getElementById('geolocation_button').addEventListener('click', function() {
 
-function onSuccess(position){
+	if (navigator.geolocation) {
+	  console.log("Browser accepts geolocation");
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	}
 
-  var longitude = position.coords.longitude;
-  var latitude = position.coords.latitude;
+	function onSuccess(position){
 
-  document.getElementById("map").setAttribute("src","http://maps.googleapis.com/maps/api/staticmap?center="+latitude+","+longitude+"&zoom=14&size=600x400&markers=color:red%7Clabel:1%7C"+latitude+","+longitude);
+	  var longitude = position.coords.longitude;
+	  var latitude = position.coords.latitude;
 
-  var container = document.getElementById("location_info");
-  var lat_html = container.createElement('p').innerHTML = "Latitude" + latitude;
-  var long_html = container.createElement('p').innerHTML = "Longitude" + longitude;
-  container.appendChild(lat_html);
-  container.appendChild(long_html);
+	  document.getElementById("map").setAttribute("src","http://maps.googleapis.com/maps/api/staticmap?center="+latitude+","+longitude+"&zoom=14&size=600x400&markers=color:red%7Clabel:1%7C"+latitude+","+longitude);
 
-};
+	  var container = document.getElementById("location_info");
+	  var lat_html = container.createElement('p').innerHTML = "Latitude" + latitude;
+	  var long_html = container.createElement('p').innerHTML = "Longitude" + longitude;
+	  container.appendChild(lat_html);
+	  container.appendChild(long_html);
 
-function onError(error){
-  console.log("Error " + error);
-};
+	};
+
+	function onError(error){
+	  console.log("Error " + error);
+	};
+
+});
