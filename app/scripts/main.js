3db63ce97d3c64a095f70f522d8af06ced38401b
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-08-05 16:22:19 +0200

    Geolocation function
    Added get geolocation JS file with event listener for a #geolocation_button to get geolocation
    Show map image on #map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,10 +13,13 @@ document.getElementById('geolocation_button').addEventListener('click', function
 	  document.getElementById("map").setAttribute("src","http://maps.googleapis.com/maps/api/staticmap?center="+latitude+","+longitude+"&zoom=14&size=600x400&markers=color:red%7Clabel:1%7C"+latitude+","+longitude);
 
 	  var container = document.getElementById("location_info");
-	  var lat_html = container.createElement('p').innerHTML = "Latitude" + latitude;
-	  var long_html = container.createElement('p').innerHTML = "Longitude" + longitude;
-	  container.appendChild(lat_html);
-	  container.appendChild(long_html);
+	  // var lat_html = container.createElement('p').innerHTML = "Latitude" + latitude;
+	  // var long_html = container.createElement('p').innerHTML = "Longitude" + longitude;
+	  // container.appendChild(lat_html);
+	  // container.appendChild(long_html);
+
+	  container.innerHTML = "Latitude: " + latitude + "<br>";
+    container.innerHTML += "Longitude: " + longitude;
 
 	};
 
